35a284a3fd23e70124046e8314686b1111d6e498
dean <deanlloyd64@gmail.com>
2020-01-18 20:13:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    let first = string[0].toUpperCase()
+    return first + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]+/g, '')
+  }
+
+  static titleize(string){
+    const untouched = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+    let stringArray = string.split(" ")
+    stringArray[0] = this.capitalize(stringArray[0])
+
+    for (const w in stringArray){
+      untouched.some((word) => word === stringArray[w]) ? null : stringArray[w] = this.capitalize(stringArray[w])
+    }
+
+    return stringArray.join(" ")
+    
+  }
 }
